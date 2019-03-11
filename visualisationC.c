88f170643bad83169090c78519062c0eb6b726fd
BlivionIaG <kev29lt@gmail.com>
2019-03-11 14:22:39 +0100

    a

--- visualisationC.c
+++ visualisationC.c
@@ -5,7 +5,7 @@ void visualisationC(float puissance_f)
    if(access(LOCK_PATH, F_OK) != -1){ // Vérifie si le fichier est disponible      
         fprintf(stderr, "File %s is locked !", CONSIGNE_PATH);
    }else{
-        FILE *lock = fopen(LOCK_PATH, "w");
+        FILE *lock = fopen(LOCK_PATH, "w"); // Crée le verrou d'accès
         FILE *sum_file_pointer = fopen(CONSIGNE_PATH, "w+"); // Ouvre le fichier de consigne
 
         if(!sum_file_pointer){ // Vérifie si on a réussi à ouvrir le fichier
@@ -20,9 +20,9 @@ void visualisationC(float puissance_f)
             fprintf(stderr, "Failed to write on %s !", CONSIGNE_PATH);    
         }
 
-        fclose(sum_file_pointer);
-        fclose(lock);
+        fclose(sum_file_pointer); // Suppression pointeur de fichier
+        fclose(lock); // Suppression pointeur de fichier
 
-        remove(LOCK_PATH);
+        remove(LOCK_PATH); // On retire le verrou
    }
 }
