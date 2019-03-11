83eed662a50c3dce6a03ad2ea3e2e233b5a2dbf6
BlivionIaG <kev29lt@gmail.com>
2019-03-11 15:10:27 +0100

    a

--- regulation.c
+++ regulation.c
@@ -2,6 +2,26 @@
 	
 float regulationTest(int regul,float csgn,float* tabT, int nT){
 	float cmd=100.0;
+
+	for(int i=0; i<nT; i++){
+		cmd = regulation(regul, csgn, tabT[i]);
+	}
+
 	return cmd;
 }
-	
+
+float regulation(int mode, float target, float temp){
+	switch(mode){
+		case 1: // ToR
+			return (temp < target)? TOR_FULL_POWER:TOR_LOW_POWER;
+		break;
+		case 2: // PID
+			
+		break;
+		default:
+			fprintf(stderr, "Error : mode %d is invalid !", mode);
+
+			return 0;
+		break;
+	}
+}
--- regulation.h
+++ regulation.h
@@ -3,6 +3,11 @@
 
 #include "define.h" 
 
+#define TOR_LOW_POWER 0
+#define TOR_FULL_POWER 40
+
 float regulationTest(int regul,float csgn,float* tabT, int nT);
+
+float regulation(int mode, float target, float temp);
 
 #endif /* REGULATION_H */
