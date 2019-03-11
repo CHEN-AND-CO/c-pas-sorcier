9f1c39b2416e17f16621a730e8ad31c62e629ccf
BlivionIaG <kev29lt@gmail.com>
2019-03-11 09:50:03 +0100

    integration

--- integration.h
+++ integration.h
@@ -1,3 +1,6 @@
+ #ifndef INTEGRATION_H
+ #define INTEGRATION_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -5,3 +8,5 @@
 #include "simulateur.h"
 
 void integrationTest(int regul,temp_t tInit,int nIterations);
+
+#endif /* INTEGRATION_H */
