7a243c0707ce2e168dfb7b5785d75a983ba846bc
HerrCraziDev <herrcrazi@gmail.com>
2019-03-11 14:24:42 +0100

    stuff

--- visualisationT.c
+++ visualisationT.c
@@ -1,17 +1,28 @@
 #include "visualisationT.h"
 
+/** 
+ * visualisationT
+ * Send temperatures to external GUI via a file
+*/
 void visualisationT(temp_t t)
 {
+    // char *line[MAX_BUFER_SIZE] = {0};
+
+    //Checking for lockfile
     if ( access("interface/.verrouData", F_OK) != -1 )
     {
-        FILE *fTemp = fopen("interface/data.txt", "w");
+        printf("[ERROR] File 'data.txt' locked\n");
+    } else {
+        FILE *fTemp = fopen("interface/data.txt", "a+");
 
         if ( !fTemp ) {
             printf("[ERROR] Failed to open file 'data.txt'\n");
-        } else {
-            fprintf(fTemp, "%.1f\n%.1f\n", t.exterieure, t.interieure);
+            return;
         }
-    } else {
-        printf("[ERROR] File 'data.txt' locked\n");
+
+        fseek(fTemp, 0, SEEK_SET);  //Set cursor to file beginning
+        fprintf(fTemp, "%.1f\n%.1f\n", t.exterieure, t.interieure);
+
+        fclose(fTemp);
     }
 }
