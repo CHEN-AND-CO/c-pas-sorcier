5d5cbf08401ccfa92874697e6fe7b4209da70b87
BlivionIaG <kev29lt@gmail.com>
2019-03-12 14:58:51 +0100

    MERGE

--- regulation.c
+++ regulation.c
@@ -4,10 +4,10 @@
 float regulationTest(int regul, float csgn, float *tabT, int nT) {
 	float cmd=100.0;
 	
-	for(size_t i = 0; i < nT; i++)
+	for(size_t i = 1; i < nT; i++)
 	{
-		cmd = regulation(regul, csgn, tabT[i], tabT[i-1]);
-		//cmd = regulation_pid(csgn,tabT[0], tabT[nT-1], 10, nT*10);
+		//cmd = regulation(regul, csgn, tabT[i], tabT[i-1]);
+		cmd = regulation_pid(csgn,tabT[0], tabT[i], tabT[i+1], 10);
 	}
 
 	return cmd;
@@ -57,7 +57,7 @@ float regulation_error_sum(float target, float current) {
 float regulation_pid(float target, float temp_start, float temp_end, int t, int to){
 	float p,i,d, pid, erreur_start, erreur_end;
 	erreur_start = target-temp_start;
-	erreur_end = target - temp_end;
+	erreur_end = target-temp_end;
 
 	p = PID_KP*(erreur_end-t/10);
 	i = PID_KI*((erreur_end-erreur_start)*(to)/2);
