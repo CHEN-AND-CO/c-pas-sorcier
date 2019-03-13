2b30d59f5d793889deae8d3a6d046ccaddde4a55
BlivionIaG <kev29lt@gmail.com>
2019-03-13 19:36:58 +0100

    Make stuff more beautiful II

--- commande.c
+++ commande.c
@@ -1,14 +1,14 @@
 #include "commande.h"
 
 void commande(usb* usb, float puis) {
-    DWORD written = 0;
-    unsigned char tx_buf = ((char)(puis/100*127))&~(1<<8);
+    DWORD written = 0; //Bytes written
+    unsigned char tx_buf = ((char)(puis/100*127))&~(1<<8); //Data to send
 
-    usb->status = FT_Write(usb->handle, &tx_buf, sizeof(&tx_buf), &written);
-
-    if (usb->status == FT_OK) {
+    usb->status = FT_Write(usb->handle, &tx_buf, sizeof(&tx_buf), &written); //USB Write
+ 
+    if (usb->status == FT_OK) { //Write success
         printf("FT_Write: Successfully written %d bytes :\t(%x)\n", written, tx_buf);
     } else {
-        perror("FT_Write: Unable to write.\n");
+        perror("FT_Write: Unable to write.\n"); //Failure
     }
 }
--- releve.c
+++ releve.c
@@ -1,9 +1,9 @@
 #include "releve.h"
 
 temp_t releve(usb *in){
-    temp_t temperature = {-1,-1};
-    DWORD RxBytes = 6;
-    DWORD BytesReceived;
+    temp_t temperature = {-1,-1}; // By default the temperatures will be -1 and -1
+    DWORD RxBytes = 6; // 3 Bytes for t_ext and 3 others for t_int
+    DWORD BytesReceived; //
     char RxBuffer[6];
     unsigned char tmp = 0;
     unsigned int i;
@@ -11,29 +11,27 @@ temp_t releve(usb *in){
 
     in->status = FT_Read(in->handle, RxBuffer, RxBytes, &BytesReceived);
     if ((in->status == FT_OK) && (BytesReceived == RxBytes)) {// FT_Read OK
-
-        
-        for(i=0; i<RxBytes; i++){
+        for(i=0; i<RxBytes; i++){ // On parcours les >6 octets reçus
             printf("DUMP:%x\t", RxBuffer[i]);
 
-            tmp = RxBuffer[i]&0xFF;
+            tmp = RxBuffer[i]&0xFF; //Pour une raison inconnue, certaines valeurs de RxBuffer sont +grandes que 8bits
             switch(tmp>>4){
-                case 0:
+                case 0: // Bits 11 à 8 de t_ext
                     t_ext |= (tmp&0x0F)<<8;
                 break;
-                case 1:
+                case 1: // Bits 7 à 4 de t_ext
                     t_ext |= (tmp&0x0F)<<4;
                 break;
-                case 2:
+                case 2: // Bits 3 à 1 de t_ext
                     t_ext |= (tmp&0x0F);
                 break;
-                case 8:
+                case 8: // Bits 11 à 8 de t_int
                     t_int |= (tmp&0x0F)<<8;
                 break;
-                case 9:
+                case 9: // Bits 7 à 4 de t_int
                     t_int |= (tmp&0x0F)<<4;
                 break;
-                case 10:
+                case 10: // Bits 3 à 1 de t_int
                     t_int |= (tmp&0x0F);
                 break;
                 default:
@@ -41,11 +39,13 @@ temp_t releve(usb *in){
             }
         }
 
-        printf("\nSOText:%d\tSOTint:%d\n", t_ext, t_int);
-        temperature.exterieure = (float)t_ext*0.04-39.64;
-        temperature.interieure = (float)t_int*0.04-39.64;
+        printf("FT_Read success, %dBytes read", BytesReceived); //Debug
+
+        printf("\nSOText:%d\tSOTint:%d\n", t_ext, t_int); //Debug
+        temperature.exterieure = (float)t_ext*0.04-39.64; //SOT_ext => t_ext
+        temperature.interieure = (float)t_int*0.04-39.64; //SOT_int => t_int
     }else {
-        fprintf(stderr, "FT_Read Error \n");
+        fprintf(stderr, "FT_Read Error \n"); //Erreur de lecture 
     }
 
     return temperature;
