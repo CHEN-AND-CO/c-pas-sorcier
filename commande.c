fa4760ffe3e9317a6304cfa33c049035cebe7217
HerrCraziDev <herrcrazi@gmail.com>
2019-03-13 15:43:26 +0100

    USB FTDI Commands

--- commande.c
+++ commande.c
@@ -1,1 +1,14 @@
 #include "commande.h"
+
+void commande(usb* usb, float puis) {
+    DWORD written = 0;
+    unsigned char tx_buf = (int)(puis/100 * 127) & ~(1<<8);
+
+    usb->status = FT_Write(usb->handle, &tx_buf, sizeof(tx_buf), &written);
+
+    if (usb->status == FT_OK) {
+        printf("FT_Write: Successfully written %d bytes :\t(%x)\n", written, tx_buf);
+    } else {
+        perror("FT_Write: Unable to write.\n");
+    }
+}
--- commande.h
+++ commande.h
@@ -1,4 +1,9 @@
 #ifndef COMMANDE_H
 #define COMMANDE_H
 
+#include "define.h"
+#include "usb.h"
+
+void commande(usb* usb, float puis);
+
 #endif /* COMMANDE_H */
